5b920d5550d3d49bd494fdefb3de08869ab38fb7
Julius Franklin <jjfranklin88@outlook.com>
2023-09-07 17:57:12 -0400

    adding footer and setting margin for large screens

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Box } from '@mui/material';
 import NavBar from './components/NavBar';
 import WorkoutDetail from './pages/WorkoutDetail';
 import Home from './pages/Home';
+import Footer from './components/Footer';
 
 const router = createBrowserRouter([
   {
@@ -18,9 +19,11 @@ const router = createBrowserRouter([
 
 function App() {
   return(
-    <Box width='400px'>
+    //setting width and margin for large screens
+    <Box width='400px' sx={{width: {xl: '1488px'}}} m='auto'>
       <NavBar />
       <RouterProvider router={router}/>
+      <Footer />
     </Box>
 
   )
