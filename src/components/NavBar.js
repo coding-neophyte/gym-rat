82b6993585b13621a78c6b7d5339c3c29d77e82d
Julius Franklin <jjfranklin88@outlook.com>
2023-09-07 18:59:20 -0400

    finishing nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,47 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { Stack } from '@mui/material'
-import Logo from '../assets/Logo.png'
-
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Stack } from '@mui/material';
+import Logo from '../assets/Logo.png';
 
 const NavBar = () => {
   return (
-    <Stack>
-        <Link to='/'>
-            <img src={Logo} alt='logo'/>
+    <Stack
+      direction="row"
+      justifyContent="space-around"
+      sx={{
+        gap: { sm: '122px', xs: '40px' },
+        mt: { sm: '32px', xs: '20px' },
+        justifyContent: 'none',
+      }}
+      px='20px' //padding
+    >
+      <Link to="/">
+        <img
+          src={Logo}
+          alt="logo"
+          style={{ width: '48px', height: '48px', margin: '0 20px' }}
+        />
+      </Link>
+      <Stack direction="row" gap="40px" fontSize="24px" alignItems="flex-end">
+        <Link
+          to="/"
+          style={{
+            textDecoration: 'none',
+            color: '#3A1212',
+            borderBottom: '3px solid #ff2625',
+          }}
+        >
+          Home
         </Link>
+        <a
+          href="#workouts"
+          style={{ textDecoration: 'none', color: '#3A1212' }}
+        >
+          Workouts
+        </a>
+      </Stack>
     </Stack>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
