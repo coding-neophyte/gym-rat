0d516869492ed75aa0e58f40691ce3af8365f9d9
Julius Franklin <jjfranklin88@outlook.com>
2023-09-07 17:49:22 -0400

    adding routes for workout detail and home pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Box } from '@mui/material';
+import NavBar from './components/NavBar';
+import WorkoutDetail from './pages/WorkoutDetail';
+import Home from './pages/Home';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: '/workout/:id',
+    element: <WorkoutDetail />
+  }
+])
 
 function App() {
-  return (
-    <div>
-        <h1>App</h1>
-    </div>
-  );
+  return(
+    <Box width='400px'>
+      <NavBar />
+      <RouterProvider router={router}/>
+    </Box>
+
+  )
+
 }
 
 export default App;
